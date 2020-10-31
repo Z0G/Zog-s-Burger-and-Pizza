f85ac42492f81b60c989e15cf981a3ece215a562
Mohammad Shoumik Raihan <37881170+Z0G@users.noreply.github.com>
2020-10-31 20:07:48 +0600

    Created Login and Register pages

--- server.js
+++ server.js
@@ -8,15 +8,26 @@ const path = require('path')
 //Assest
 app.use(express.static('public'))
 
+// set template engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req,res) => {
     res.render('home')
 })
 
-// set template engine
-app.use(expressLayout)
-app.set('views', path.join(__dirname, '/resources/views'))
+app.get('/cart', (req,res) => {
+    res.render('customers/cart')
+})
 
-app.set('view engine', 'ejs')
+app.get('/login', (req,res) => {
+    res.render('auth/login')
+})
+
+app.get('/register', (req,res) => {
+    res.render('auth/register')
+})
 
 app.listen(PORT, ()=> {
     console.log(`Listening on port ${PORT}`)
